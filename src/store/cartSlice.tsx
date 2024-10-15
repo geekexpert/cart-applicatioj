0bba3fdf0f7d49a7bed7d4591e54a364ccb59251
john.silva <john.silva@datacom.com>
2024-10-15 23:38:47 +1300

    cart application initial coding structure  - react ts, redux

---
+++ src/store/cartSlice.tsx
@@ -0,0 +1,24 @@
+// src/store/counterSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
